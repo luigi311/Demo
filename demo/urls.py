004f286133aed21d87902ca5402a5a357046d287
Luis Garcia <luigi311.lg@gmail.com>
2015-09-17 13:56:59 -0600

    Finished step 4 in the tutorial. Created a home page with basic information

--- demo/urls.py
+++ demo/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from polls.views import HomeView
 
 urlpatterns = [
+    url(r'^$', HomeView.as_view(), name = 'home'),
+    url(r'^polls/', include('polls.urls', namespace="polls")),
     url(r'^admin/', include(admin.site.urls)),
 ]
